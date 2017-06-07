df5ebcd095fb2848b1709be693d2caa9d6b8f765
VoidWatcher <VoidWatcher@users.noreply.github.com>
2017-06-07 18:56:44 +0800

    2

--- main/main.js
+++ main/main.js
@@ -2,6 +2,7 @@ module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
 // Write your code here
+o) 最大值 = ${sequence.maximum()}
 
 `);
 };
@@ -15,13 +16,24 @@ class Sequence {
   minimum() {
     // Write your code here
     var minimum_number = this.input[0];
-    for (var i = 0; i < this.input.length; i ++){
-      if (this.input[i] < minimum_number){
+    for (var i = 0; i < this.input.length; i ++) {
+      if (this.input[i] < minimum_number) {
         minimum_number = this.input[i];
       }  
     }
     return minimum_number;
   }
-
+  
   // Write your code here
+  maximum() {
+    var maximum_number = this.input[0];
+    for (var i = 0; i < this.input.length; i ++) {
+      if (this.input[i] > maximum_number) {
+        maximum_number = this.input[i];
+      }  
+    }
+    return maximum_number;
+  }
+  
+  
 }

7ed24e70497945be692a9652434e5808ab848e2c
VoidWatcher <VoidWatcher@users.noreply.github.com>
2017-06-07 18:24:24 +0800

    1

--- main/main.js
+++ main/main.js
@@ -2,16 +2,25 @@ module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
 // Write your code here
+
 `);
 };
 
 class Sequence {
   constructor(input) {
     // Write your code here
+    this.input = input;
   }
 
   minimum() {
     // Write your code here
+    var minimum_number = this.input[0];
+    for (var i = 0; i < this.input.length; i ++){
+      if (this.input[i] < minimum_number){
+        minimum_number = this.input[i];
+      }  
+    }
+    return minimum_number;
   }
 
   // Write your code here
